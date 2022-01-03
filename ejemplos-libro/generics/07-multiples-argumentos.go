2ad769b0a4eebc0da19b03d8213097cb95e161e5
Mario Macias <mmaciasl@redhat.com>
2022-01-03 20:00:19 +0100

    alguna corrección en multimapa

--- ejemplos-libro/generics/07-multiples-argumentos.go
+++ ejemplos-libro/generics/07-multiples-argumentos.go
@@ -6,37 +6,37 @@ import (
 )
 
 type MultiMapa[C comparable, V any] struct {
-	mapa map[C][]V
+	datos map[C][]V
 }
 
 // recordar: las restricciones solo se especifican en la definicion
 func (m *MultiMapa[C, V]) Pon(clave C, valor V) {
-	if m.mapa == nil {
-		m.mapa = map[C][]V{}
+	if m.datos == nil {
+		m.datos = map[C][]V{}
 	}
-	m.mapa[clave] = append(m.mapa[clave], valor)
+	m.datos[clave] = append(m.datos[clave], valor)
 }
 
 func (m *MultiMapa[C, V]) Lee(clave C) ([]V, bool) {
-	if m.mapa == nil {
+	if m.datos == nil {
 		return nil, false
 	}
-	valor, ok := m.mapa[clave]
+	valor, ok := m.datos[clave]
 	return valor, ok
 }
 
 func main() {
-	pueblos := MultiMapa[string, string]{}
-	pueblos.Pon("Colombia", "Cali")
-	pueblos.Pon("España", "Cachorrilla")
-	pueblos.Pon("Colombia", "Ibagué")
-	pueblos.Pon("Argentina", "La Puerta")
-	pueblos.Pon("España", "Vinuesa")
+	paises := MultiMapa[string, string]{}
+	paises.Pon("Colombia", "Cali")
+	paises.Pon("España", "Cachorrilla")
+	paises.Pon("Colombia", "Ibagué")
+	paises.Pon("Argentina", "La Puerta")
+	paises.Pon("España", "Vinuesa")
 
 	for _, pais := range []string{
 		"Colombia", "Perú", "Argentina", "España", "Malta",
 	} {
-		if pueblos, ok := pueblos.Lee(pais); ok {
+		if pueblos, ok := paises.Lee(pais); ok {
 			fmt.Printf("Pueblos de %s: %v\n", pais, pueblos)
 		} else {
 			fmt.Println("No se encontraron pueblos en", pais)
